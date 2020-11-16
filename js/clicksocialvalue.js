1558d25c3094a85385d0c18205863c25619ddfc1
tomzhenghh <zhh081119@163.com>
2020-11-16 18:19:37 +0800

    Site updated: 2020-11-16 18:19:36

--- js/clicksocialvalue.js
+++ js/clicksocialvalue.js
@@ -15,4 +15,4 @@
         span.style.cssText = ['z-index: 9999999; position: absolute; font-weight: bold; color: #ff6651; top: ', y - 20, 'px; left: ', x, 'px;'].join('');
         document.body.appendChild(span);
         animate(span);
-    });
+    })
